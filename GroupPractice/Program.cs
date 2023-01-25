e1fcc169720dcc6441293d67657149826c157407
camdixon132 <camdixon132@gmail.com>
2023-01-25 16:46:27 -0500

    cw name commit

--- GroupPractice/Program.cs
+++ GroupPractice/Program.cs
@@ -1,10 +1,15 @@
 ﻿using GroupPractice;
-List <Person> Names = new List<Person>();
-new Person("Michael Blackburn", 28);
-new Person("Mourad Al-fadil", 26);
-new Person("Cameron Dixon", 22);
+List<Person> Names = new List<Person>()
+{
+    new Person("Michael Blackburn", 28),
+    new Person("Mourad Al-fadil", 26),
+    new Person("Cameron Dixon", 22)
+};
 
-
+foreach (Person p in Names)
+{
+    Console.WriteLine($"{p.Name}, {p.Age}");
+}
 
 
 
