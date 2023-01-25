8b3a2fef7827105a66d279c41efb48978133caef
Mourad Alfadil <mouradalfadil@gmail.com>
2023-01-25 16:21:36 -0500

    added Person class with properties and constructor

---
+++ GroupPractice/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupPractice
+{
+    internal class Person
+    {
+        //properties
+        internal string Name { get; set; }
+        internal int Age { get; set; } 
+        //constructor 
+        internal Person(string _name, int _age)
+        {
+            Name= _name;
+            Age= _age;
+        }
+    }
+}
